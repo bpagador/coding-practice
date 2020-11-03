b897d9caf924003e9784d990a2829e2d6b3ebec7
Briseida Pagador <bpagador@gmail.com>
2020-11-03 13:05:01 -0800

    instructions in

---
+++ Challenges/arrayOfArrays.js
@@ -0,0 +1,4 @@
+// - Write a function that accepts arrays of arrays 
+// - Translate pairs of value into key value pairs 
+// - sample input: [[‘foo’, ‘one’], [‘bar’, ‘two’], [‘baz’, ‘three’]]
+// - sample output: { ‘foo’: ‘one’, ‘bar’: ‘two’, ‘baz’: ‘three’ }
